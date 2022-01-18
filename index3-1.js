7fdd0ba2ebdfef3d604b076c6ab52cc95dc966d9
ugm158 <ugm158@gmail.com>
2022-01-18 17:00:30 +0800

    v3-1-2

--- index3-1.js
+++ index3-1.js
@@ -1,6 +1,13 @@
-let scroe = 60;
+let scroe = 85;
+/*
 if(scroe >= 60){
     console.log("您的分數是及格的！");
 } else {
     console.log("您的分數是不及格的！");
+}
+*/
+if(scroe >= 90){
+    console.log("您的分數是A級！");
+} else if(scroe >= 80){
+    console.log("您的分數是B級！");
 }
