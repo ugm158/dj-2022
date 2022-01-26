8b31778000a1d894bc2b1cf0733b45d3c25f74ca
ugm158 <ugm158@gmail.com>
2022-01-26 13:31:05 +0800

    r5Function_2

---
+++ r5Function_2.js
@@ -0,0 +1,11 @@
+
+/**
+ class 使用
+ */
+ function r5Function(){
+    let head = Sheet.getHead('工作表4');
+    let head_3 = Sheet.getHead('工作表3');
+
+    let data = Sheet.getData('工作表4');
+    console.log(data.length, data);
+  }
