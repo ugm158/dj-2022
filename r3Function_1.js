c814dbbcdeef6e2171797da23e3fdfb2d3b0a422
ugm158 <ugm158@gmail.com>
2022-01-26 10:13:02 +0800

    r3Function_2

--- r3Function_1.js
+++ r3Function_1.js
@@ -5,7 +5,7 @@
  function r3Function(){
     let ss = SpreadsheetApp.getActiveSpreadsheet();
     let ws = ss.getSheetByName('工作表4');
-    let head = ws.getSheetValues(1, 1, 1, ws.getLastColumn);
+    let head = ws.getSheetValues(1, 1, 1, ws.getLastColumn());
     console.log(head);
 
   }
