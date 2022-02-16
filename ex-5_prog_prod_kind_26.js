a81d16d01ad6c1c33df477859e85a0bdea747e79
ugm158 <ugm158@gmail.com>
2022-02-16 15:04:57 +0800

    ex-5_prog_prod_kind_26

---
+++ ex-5_prog_prod_kind_26.js
@@ -0,0 +1,35 @@
+
+/*=====================================
+  商品類別 管理
+=====================================*/
+function prod_kind(e){
+
+    // 商品類別 表單
+    let content = Sheet.render('prod_kind', {global: global});
+
+
+    // 主樣板
+    return Sheet.render('index', {content: content, menu: menu}, global['網站標題']);
+  }
+
+  /*=====================================
+    寫入商品類別
+    { '商品類別': '招牌', op: 'set_prod_kind' }
+  =====================================*/
+  function set_prod_kind(e){
+    // 開始寫入工作表
+    let sheet = '商品類別';
+    let ws = Sheet.getWs(sheet);
+    let rowIndex = ws.getLastRow() + 1;
+    Sheet.setCellData(sheet, rowIndex, 1, e.parameter.商品類別, '文字');
+
+    // 回傳訊息
+
+    console.log(e.parameter);
+
+    let content = '';
+
+    // 主樣板
+    return Sheet.render('index', {content: content, menu: menu}, global['網站標題']);
+
+  }
